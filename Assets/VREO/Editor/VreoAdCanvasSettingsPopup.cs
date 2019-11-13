079ed300eddf5fea613eb71b354065c93dd94045
Ivan Tustanivskyi <itustanivsky@ninevastudios.com>
2019-11-13 09:49:04 +0200

    #8 fix register popup closing behavior

--- Assets/VREO/Editor/VreoAdCanvasSettingsPopup.cs
+++ Assets/VREO/Editor/VreoAdCanvasSettingsPopup.cs
@@ -62,6 +62,10 @@ public class VreoAdCanvasSettingsPopup : EditorWindow
                 Close();
             }
         }
+        else
+        {
+            Close();
+        }
     }
 
     void OnGUI()
